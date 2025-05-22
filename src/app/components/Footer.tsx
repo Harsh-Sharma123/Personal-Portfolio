bf126a92b95236caa296fcb1fc99beba0e80e95b
Harsh Sharma <hs804506@gmail.com>
2025-05-22 21:05:17 +0530

    typing text animation added in hero section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,14 +3,16 @@ import { FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <div className='mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 px-6 md:px-0 flex justify-between items-center'>
-        <h1 className='text-2xl font-bold'>Harsh Sharma</h1>
+    <div className='mt-12 text-white/70 py-8 max-w-[1000px]  mx-auto border-t border-gray-700 pt-4 px-6 md:px-0 flex justify-between items-center'>
+        <h1 className='text-xl lg:text-2xl font-bold'>Harsh Sharma</h1>
 
-        <div className='flex space-x-8 mt-4'>
-            <a href="#" className='hover:text-gray-300'>
+        <div className='font-bold'>Made with Love❤️</div>
+
+        <div className='flex space-x-8 items-center justify-center'>
+            <a href="https://www.linkedin.com/in/harsh-sharma-a67254195/" className='hover:text-gray-300'>
                 <FaLinkedin size={24} />
             </a>
-            <a href="#" className='hover:text-gray-300'>
+            <a href="https://x.com/HarshSharma7" className='hover:text-gray-300'>
                 <FaTwitter size={24} />
             </a>
             <a href="#" className='hover:text-gray-300'>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 
 import cursor from '../assets/icon1.png';
 import lightning from '../assets/icon2.png';
 import Image from 'next/image';
 import profilepicture from '../assets/profilepic.png';
+import { Typewriter } from 'react-simple-typewriter';
+import { CloudDownload } from 'lucide-react';
 
 const handleDownload = () => {
     const link = document.createElement('a');
@@ -15,9 +17,45 @@ const handleDownload = () => {
     link.click();
 }
 
+
+const titles = [' Software Developer', ' Full Stack Developer', ' Tech Enthusiast'];
+
+
+function useTypingEffect(words: string[], speed = 100, pause = 1000) {
+    const [index, setIndex] = useState(0);
+    const [displayedText, setDisplayedText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+  
+    useEffect(() => {
+      const current = words[index % words.length];
+  
+      let typingTimeout = setTimeout(() => {
+        if (isDeleting) {
+          setDisplayedText((prev) => prev.slice(0, -1));
+        } else {
+          setDisplayedText((prev) => current.slice(0, prev.length + 1));
+        }
+  
+        if (!isDeleting && displayedText === current) {
+          setTimeout(() => setIsDeleting(true), pause);
+        } else if (isDeleting && displayedText === '') {
+          setIsDeleting(false);
+          setIndex((prev) => prev + 1);
+        }
+      }, isDeleting ? speed / 2 : speed);
+  
+      return () => clearTimeout(typingTimeout);
+    }, [displayedText, isDeleting, index, words, speed, pause]);
+  
+    return displayedText;
+  }  
+
+
+
 export const Hero = () => {
+    const text = useTypingEffect(titles, 100, 1500);
     return (
-        <div className='py-24 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)] px-4'>
+        <div className='py-12 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)] px-4'>
 
             <div className='absolute rounded-[50%] sm:w-[150vw] sm:h-[90vh] w-[3000px] h-[1300px] sm:w-auto sm:h-auto top-[400px] sm:top-[450px] md:top-[550px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_80%,#2B1942)]'></div>
 
@@ -68,9 +106,34 @@ export const Hero = () => {
                     />
                 </motion.div>
 
-                <p className='text-center text-lg  md:text-2xl max-w-[500px] mx-auto mt-10 text-white/80 px-2'>
-                    I am a Software Developer
+                <p className='text-center text-lg  md:text-2xl max-w-[500px] mx-auto mt-8 text-white/80 px-2'>
+                    I am a
+                    <span className='text-semibold'>
+
+                        
+                            
+                            {text}
+                        
+
+                    {/* <Typewriter
+                        words={[' Software Developer', ' Full Stack Developer', ' Tech Enthusiast']}
+                        loop={0}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        /> */}
+                        </span>
                 </p>
+{/* 
+                <div className='mx-auto mt-8 text-center'>
+                    <div className='flex justify-center items-center'>
+                        <button className='border-2 border-gray-500 hover:cursor-pointer text-white/80 px-4 py-2 rounded-lg flex justify-center items-center group'>Download Resume
+                            <CloudDownload className='w-5 h-5 ml-2 text-white/80 group-hover:animate-bounce' />
+                        </button>
+                    </div>
+                </div> */}
 
                 {/* <div className='text-center mt-6'><span className='inline-block cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 mx-auto mt-6 border-2 rounded-xl text-xl font-semibold border-gray-700 px-12 py-2'>Download Resume</span></div> */}
 
